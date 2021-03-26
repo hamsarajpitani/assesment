7114e56f64b8382ffed14ca9e43c5825e4468f0e
hamsarajpitani <pitanihamsaraj@gmail.com>
2021-03-26 16:16:21 +0530

    todoadded

--- src/App.js
+++ src/App.js
@@ -11,11 +11,45 @@ import { login, logout, selectUser } from "./features/userSlice";
 import Todos from "./Todos";
 import Gallary from "./screens/gallary/Gallary";
 import Navbar from './Navbar';
-
+import Todolist from './Todos.jsx';
 
 function App() {
   // let user = null;
   let user = useSelector(selectUser);
+
+  const DATA = [
+    {
+      id: 1,
+      title: "Loard Of The Rings",
+      category: "Movies",
+      date: "",
+      isDone: false
+    },
+    { id: 2, title: "God Father I", category: "Movies", date: "", isDone: false },
+    {
+      id: 3,
+      title: "Seven Angry Man",
+      category: "Movies",
+      date: "",
+      isDone: false
+    },
+    { id: 4, title: "Pulp Fiction", category: "Movies", date: "", isDone: true },
+    {
+      id: 5,
+      title: "The Brothers Karamazov",
+      category: "Books",
+      date: "",
+      isDone: true
+    },
+    {
+      id: 6,
+      title: "The Master and Margarita",
+      category: "Books",
+      date: "",
+      isDone: false
+    },
+    { id: 7, title: "Don Quixote", category: "Books", date: "", isDone: false }
+  ];
 
   const dispatch = useDispatch();
 
@@ -54,7 +88,7 @@ function App() {
               <HomeScreen/>
             </Route>
             <Route exact path="/todos">
-              <Todos/>
+            <Todolist tasks={DATA} />
             </Route>
             <Route exact path="/gallary">
               <Gallary/>
